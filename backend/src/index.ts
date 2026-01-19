e2363221a542f994f4c5015185f35de71edecd79
Nakul <nakulgite08@gmail.com>
2026-01-19 14:00:16 +0530

    websocket broadcasting messages logic completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,34 +6,99 @@ const PORT = 8080;
 const wss = new WebSocketServer({port : PORT}); 
 console.log(`Websocket sever running on ws://localhost:${PORT}`);
 
-//store all the connected clients
-const clients = new Set<WebSocket>();  //stores the unique values
+
+// roomId -> set of sockets
+const rooms = new Map<string, Set<WebSocket>>(); //For each room name, store a SET of sockets connected to that room
+/*
+rooms = {
+  "general" → Set(socketA, socketB),
+  "random"  → Set(socketC)
+}
+*/
+
+// socket -> roomId
+const socketRoom = new Map<WebSocket, string>();  //For each socket, remember which room it belongs to
+/*
+socketRoom = {
+  socketA → "general",
+  socketB → "general",
+  socketC → "random"
+}
+*/
+
+//	To broadcast → you need room → sockets
+// 	To clean up → you need socket → room
 
 wss.on("connection", (socket)=>{
     console.log("new client connected");
-    clients.add(socket);   //added to the set
 
     //when message is received from the client 
     socket.on("message", (data)=>{
-        const message = data.toString();   //the data is raw binary data so convert it to string first
-        console.log("Received", message);
-        
-        //Broadcast this message to all the connected clients
-        for(const client of clients){
-            if(client.readyState === WebSocket.OPEN){
-                client.send(message);
+        try {
+            const parsed = JSON.parse(data.toString());  //parses the json string obj and convert it to obj
+            const {type, roomId, payload} = parsed;  
+
+            //join room logic
+            if(type === "join"){
+                if(!roomId) return;
+                // Remove socket from old room (if any)
+                const prevRoom = socketRoom.get(socket);
+                if(prevRoom){
+                    rooms.get(prevRoom)?.delete(socket);
+                }
+
+                // Add to new room
+                if(!rooms.has(roomId)){
+                    rooms.set(roomId, new Set());
+                    //rooms = {
+                    //  "general" → Set()
+                    // }
+                }
+                rooms.get(roomId)?.add(socket);  
+                socketRoom.set(socket, roomId);
+
+                console.log(`Client joined room: ${roomId}`);
+                socket.send(
+                    JSON.stringify({
+                        type: "system",
+                        message: `Joined room ${roomId}`,
+                    })
+                );
             }
-        }
-    });
 
-    //handle the close
-    socket.on("close", ()=>{
-        console.log("Client disconnected");
-        clients.delete(socket);
-    });
+            //send message in the room
+            if(type === "message"){
+                const currentRoom = socketRoom.get(socket);  //this will give the name of room for which the current socket belongs
+                if(!currentRoom) return;
 
-    //error handled
-    socket.on('error', (err)=>{
-        console.log("Socket error", err);
-    })
+                const clients = rooms.get(currentRoom);  //this will return the set of sockets belong to current room
+                if(!clients) return;
+                for(const client of clients){
+                    if(client.readyState === WebSocket.OPEN){
+                        client.send(
+                            JSON.stringify({
+                                type : "message", 
+                                roomId : currentRoom,
+                                payload,
+                            })
+                        )
+                    };
+                };
+            }
+
+        } catch (error) {
+            console.error("Invalid message format");
+        };
+
+
+        //handle disconnect 
+        socket.on('close', ()=>{
+            const roomId = socketRoom.get(socket);
+            if(roomId){
+                rooms.get(roomId)?.delete(socket);
+                socketRoom.delete(socket);
+                console.log(`Client left room: ${roomId}`);
+            }
+        })
+    });
 })
