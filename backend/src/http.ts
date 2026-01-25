0b943759274ca7fbb9309d9b0b7e1de285793fa2
Nakul <nakulgite08@gmail.com>
2026-01-25 18:58:30 +0530

    added cors url

--- backend/src/http.ts
+++ backend/src/http.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ["http://localhost:3000", 'http://127.0.0.1:5500'],
+    origin: ["http://localhost:3000", 'http://127.0.0.1:5500', "http://localhost:5173"],
     credentials: true,
 }));
 
